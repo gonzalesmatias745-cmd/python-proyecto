25a9026c2e21e87ffbf1af5c5428049f602129e7
gonzalesmatias745-cmd <gonzalesmatias745@gmail.com>
2025-08-02 11:46:23 -0500

    primeros avances

---
+++ variablse_y_tipos.py
@@ -0,0 +1,10 @@
+# Variables y tipos de datos
+nombre = "Samuel"
+edad = 20
+altura = 1.75
+es_estudiante = True
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Altura:", altura)
+print("¿Es estudiante?", es_estudiante)
